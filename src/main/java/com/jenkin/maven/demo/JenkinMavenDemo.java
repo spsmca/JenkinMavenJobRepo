7cee7c2fb6db23f005fc0accaa38e24bcf79de56
spsmca <sps_mca@rediffmail.com>
2020-09-18 22:56:30 +0530

    Second Commit

--- src/main/java/com/jenkin/maven/demo/JenkinMavenDemo.java
+++ src/main/java/com/jenkin/maven/demo/JenkinMavenDemo.java
@@ -4,7 +4,7 @@ public class JenkinMavenDemo {
 
 	public static void main(String[] args) {
 		System.out.println("Begin===>JenkinMavenDemo.main()....");
-		for(int i=1; i<6; ++i) {
+		for(int i=1; i<11; ++i) {
 			System.out.println("Hey Philips===> counter=" + i);
 		}
 		System.out.println("End===>JenkinMavenDemo.main()....");
